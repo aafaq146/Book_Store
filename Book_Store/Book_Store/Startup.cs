3cfa55816ebfc5f09ae4f86a70b7f72152aaa1f8
Aafaq Ahmad <aafaq146@gmail.com>
2021-10-27 15:41:24 +0500

    updated

--- Book_Store/Book_Store/Startup.cs
+++ Book_Store/Book_Store/Startup.cs
@@ -16,6 +16,8 @@ namespace Book_Store
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddRazorPages()
+       .AddRazorRuntimeCompilation();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -26,13 +28,21 @@ namespace Book_Store
                 app.UseDeveloperExceptionPage();
             }
 
+           
             app.UseRouting();
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
+                endpoints.Map("/", async context =>
                 {
-                    await context.Response.WriteAsync("Hello World!");
+                    await context.Response.WriteAsync("Hello Aafaq!");
+                });
+            });
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.Map("/aafaq", async context =>
+                {
+                    await context.Response.WriteAsync("Hello Ahmad!");
                 });
             });
         }
